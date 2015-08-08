6ff463a4ba93cf4312944b28d353d824441ad410
totuworld <totuworld@gmail.com>
2015-08-08 17:17:37 +0900

    update usercore

--- models/usercore.js
+++ models/usercore.js
@@ -2,17 +2,17 @@
 
 module.exports = function(sequelize, DataTypes) {
   var usercore = sequelize.define('usercore', {
-    no: {type : DataTypes.INTEGER.UNSIGNED,  primaryKey: true}
-    , id : {type : DataTypes.STRING(14)}
-    , gems : {type : DataTypes.INTEGER(6).UNSIGNED, defaultValue:0}
-    , coins : {type : DataTypes.INTEGER.UNSIGNED, defaultValue:0}
-    , hearts : {type : DataTypes.INTEGER(4).UNSIGNED, defaultValue:0}
-    , highScore : {type : DataTypes.INTEGER.UNSIGNED, defaultValue:0}
-    , loginTime : {type : DataTypes.INTEGER(10).UNSIGNED, defaultValue:0}
-  }, 
-  {
-    timestamps:false,
-    tableName: 'usercore'
+	  no : { type : Sequelize.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true}
+	  , id : { type : Sequelize.STRING(14) }
+	  , gems : { type : Sequelize.INTEGER(5).UNSIGNED, defaultValue: 0}
+	  , coins : { type : Sequelize.INTEGER.UNSIGNED, defaultValue: 0}
+	  , hearts : { type : Sequelize.INTEGER(3).UNSIGNED, defaultValue: 0}
+	  , highScore : { type : Sequelize.INTEGER.UNSIGNED, defaultValue: 0}
+	  , loginTime : { type : Sequelize.DATE, defaultValue: '2002-06-05 00:00:00'
+		  , get:function(){var convertTime=new Date(this.getDataValue('loginTime')); return convertTime.getTime()/1000;}}
+  }, {
+	  timestamps: false,
+	  tableName: 'usercore'
   });
   return usercore;
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
+/// <reference path="../typings/express/express.d.ts"/>
+/// <reference path="../typings/async/async.d.ts"/>
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var models = require('../models');
+var async = require('async');
+
+
+
+/* POST 사용자 아이디를 등록할 때 사용한다. */
+router.post('/add/:userID', function(req, res) {
+  
+  //아이디 중복 확인
+  function CheckIsHaveID(callback) {
+    models.usercore.find({where:{id:req.params.userID}})
+    .then(function(findUserCoreData) {
+      callback( !(findUserCoreData == null || findUserCoreData == undefined) );
+    });
+  }
+  //아이디를 등록한다.
+  function CreateAccount(callback) {
+    models.usercore
+    .create({id:req.parmas.userID, gems:20, coins:1000, hearts:5})
+    .then(function(createdUserCore) {
+      callback(null, createdUserCore.no);
+    });
+  }
+
+  async.series([
+    CheckIsHaveID,
+    CreateAccount
+  ], function(err, results) {
+    if(err) {
+      //err:아이디가 중복되는 경우.
+      res.send('exist');
+    }
+    else {
+      //완료 결과 전송.
+      res.send('done0'+results[1]);
+    }
+  });
+  
 });
+
+
 
 module.exports = router;
